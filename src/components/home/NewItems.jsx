19a84742447caf4d7798201ea1583612c61b113a
perrinbm97 <perrinbm08@gmail.com>
2026-05-25 14:03:07 -0400

    Reactive Carousel

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import ReactOwlCarousel from "react-owl-carousel";
 
 const NewItems = () => {
+  const [nft, setNft] = useState([]);
+  const [loading, setLoading] = useState();
+
+  async function fetchData() {
+    const { data } = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems",
+    );
+    console.log(data);
+    setNft(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const carouselOptions = {
+    loop: true,
+    dots: false,
+    nav: true,
+    lazyLoad: true,
+    responsive: {
+      0: { items: 1 },
+      600: { items: 2 },
+      1000: { items: 3 },
+      1200: { items: 4 },
+    },
+  };
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,7 +42,74 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+
+          {nft.length > 0 && (
+            <ReactOwlCarousel
+              className="owl-theme"
+              {...carouselOptions}
+              key={loading ? "loading" : "loaded"}
+            >
+              {nft.map((card) => (
+                <div className="px-2" key={card.id}>
+                  <div className="nft__item">
+                    <div className="author_list_pp">
+                      <Link
+                        to={`/author/${card.authorId}`}
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        title="Creator: Monica Lucas"
+                      >
+                        <img className="lazy" src={card.authorImage} alt="" />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="de_countdown">
+                      {Date.now - card.expiryDate}
+                    </div>
+
+                    <div className="nft__item_wrap">
+                      <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                          <button>Buy Now</button>
+                          <div className="nft__item_share">
+                            <h4>Share</h4>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-facebook fa-lg"></i>
+                            </a>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-twitter fa-lg"></i>
+                            </a>
+                            <a href="">
+                              <i className="fa fa-envelope fa-lg"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+
+                      <Link to={`/item-details/${card.nftId}`}>
+                        <img
+                          src={card.nftImage}
+                          className="lazy nft__item_preview"
+                          alt=""
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft__item_info">
+                      <Link to={`/item-details/${card.nftId}`}>
+                        <h4>{card.title}</h4>
+                      </Link>
+                      <div className="nft__item_price">{card.price} ETH</div>
+                      <div className="nft__item_like">
+                        <i className="fa fa-heart"></i>
+                        <span>{card.likes}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </ReactOwlCarousel>
+          )}
+          {/* {new Array(4).fill(0).map((_, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
@@ -69,7 +164,7 @@ const NewItems = () => {
                 </div>
               </div>
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
     </section>
