2945b92588db9555730be2726cb008f4f686e2aa
perrinbm97 <perrinbm08@gmail.com>
2026-05-31 11:28:06 -0400

    API+Follow

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const Author = () => {
+  const { id } = useParams();
+  const [author, setAuthor] = useState([]);
+  const [following, setFollowing] = useState(false);
+
+  async function fetchAuthor() {
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`,
+    );
+    setAuthor(data);
+    console.log(data);
+  }
+
+  function follow() {
+    setFollowing((prev) => !prev);
+    if (!following) {
+      author.followers++;
+    } else {
+      author.followers--;
+    }
+  }
+
+  useEffect(() => {
+    fetchAuthor();
+  }, []);
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -25,15 +51,17 @@ const Author = () => {
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
                     <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                      <img src={author.authorImage} alt="" />
 
                       <i className="fa fa-check"></i>
                       <div className="profile_name">
                         <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
+                          {author.authorName}
+                          <span className="profile_username">
+                            @{author.tag}
+                          </span>
                           <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                            {author.address}
                           </span>
                           <button id="btn_copy" title="Copy Text">
                             Copy
@@ -44,9 +72,15 @@ const Author = () => {
                   </div>
                   <div className="profile_follow de-flex">
                     <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
+                      <div className="profile_follower">
+                        {author.followers} followers
+                      </div>
+                      <Link
+                        to="#"
+                        onClick={() => follow()}
+                        className="btn-main"
+                      >
+                        {following ? "Unfollow" : "Follow"}
                       </Link>
                     </div>
                   </div>
