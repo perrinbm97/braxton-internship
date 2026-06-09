15ea02268fcbcb771a289f8bdf08b3a5e09c4706
perrinbm97 <perrinbm08@gmail.com>
2026-06-08 23:02:25 -0400

    Bug Fix3

--- src/components/UI/nftCard.jsx
+++ src/components/UI/nftCard.jsx
@@ -34,15 +34,15 @@ const NftCard = ({ data, loading, profile }) => {
               <button>Buy Now</button>
               <div className="nft__item_share">
                 <h4>Share</h4>
-                <a href="#" target="_blank" rel="noreferrer">
+                <button target="_blank" rel="noreferrer">
                   <i className="fa fa-facebook fa-lg"></i>
-                </a>
-                <a href="#" target="_blank" rel="noreferrer">
+                </button>
+                <button target="_blank" rel="noreferrer">
                   <i className="fa fa-twitter fa-lg"></i>
-                </a>
-                <a href="#">
+                </button>
+                <button>
                   <i className="fa fa-envelope fa-lg"></i>
-                </a>
+                </button>
               </div>
             </div>
           </div>
